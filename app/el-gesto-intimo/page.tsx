bea42b078da40955d33fd9340a754c59ecbf9aa0
Mauro Pretti <mauropretti@192.168.1.6>
2026-05-26 14:08:24 -0300

    update

--- app/el-gesto-intimo/page.tsx
+++ app/el-gesto-intimo/page.tsx
@@ -318,17 +318,15 @@ export default function GestoIntimoPage() {
                 {/* HORIZONTAL DRIFT */}
 
                 <div
-  className="
-    overflow-x-auto
-    md:overflow-hidden
+                  className="
+                    overflow-x-auto
+                    md:overflow-hidden
 
-    w-full
+                    w-full
 
-    scrollbar-hide
-
-    touch-pan-y
-  "
->
+                    scrollbar-hide gesto-mobile-scroll
+                  "
+                >
 
                   <div
                     className="
