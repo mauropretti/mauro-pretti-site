fb8eaa8f85a0050ceafec1c472aa7c158e97aea7
Mauro Pretti <mauropretti@192.168.1.6>
2026-05-27 01:01:28 -0300

    Add manual project ordering

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,7 +10,7 @@ import {urlFor} from '@/sanity/image'
 async function getProjects() {
 
   return client.fetch(`
-    *[_type == "project"] | order(_createdAt desc) {
+    *[_type == "project"] | order(order asc) {
 
       _id,
       title,
