52206e10154dbe69986bb3b8185c0ebba8d23d2c
Mauro Pretti <mauropretti@192.168.1.19>
2026-05-31 20:59:28 -0300

    update

--- app/api/webhooks/mercadopago/route.ts
+++ app/api/webhooks/mercadopago/route.ts
@@ -1,6 +1,14 @@
 import {NextResponse} from 'next/server'
 import {writeClient} from '@/sanity/writeClient'
 
+export async function GET() {
+
+  return NextResponse.json({
+    webhook: true,
+  })
+
+}
+
 export async function POST(req: Request) {
 
   const text = await req.text()
