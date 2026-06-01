5408d871f86806763089381b91037f5b2eae869f
Mauro Pretti <mauropretti@192.168.1.19>
2026-05-31 23:13:40 -0300

    update

--- app/api/webhooks/mercadopago/route.ts
+++ app/api/webhooks/mercadopago/route.ts
@@ -2,6 +2,16 @@ import {sendOrderEmails} from '@/app/lib/sendOrderEmails'
 import {NextResponse} from 'next/server'
 import {writeClient} from '@/sanity/writeClient'
 
+import {
+  MercadoPagoConfig,
+  Payment,
+} from 'mercadopago'
+
+const client = new MercadoPagoConfig({
+  accessToken:
+    process.env.MERCADOPAGO_ACCESS_TOKEN!,
+})
+
 export async function GET() {
 
   return NextResponse.json({
@@ -32,29 +42,52 @@ export async function POST(req: Request) {
 
   }
 
+  const paymentClient =
+    new Payment(client)
+
+  const payment =
+    await paymentClient.get({
+      id: Number(paymentId),
+    })
+
+  console.log(
+    JSON.stringify(payment, null, 2)
+  )
+
   await writeClient.create({
-  _type: 'order',
-  paymentId: String(paymentId),
-  status: body.action,
-  customerName: 'Webhook',
-  customerEmail: 'webhook@test.com',
-  customerPhone: '',
-  artwork: 'Webhook',
-  size: 'Webhook',
-  price: 0,
-  createdAt: new Date().toISOString(),
-})
+    _type: 'order',
 
-await sendOrderEmails({
-  customerName: 'Webhook',
-  customerEmail: 'webhook@test.com',
-  customerPhone: '',
-  artwork: 'Prueba',
-  size: '30x45',
-  price: 1000,
-  paymentId: String(paymentId),
-})
+    paymentId: String(paymentId),
 
-return NextResponse.json({
-  success: true,
-})}
+    status: body.action,
+
+    customerName: 'Webhook',
+
+    customerEmail: 'webhook@test.com',
+
+    customerPhone: '',
+
+    artwork: 'Webhook',
+
+    size: 'Webhook',
+
+    price: 0,
+
+    createdAt: new Date().toISOString(),
+  })
+
+  await sendOrderEmails({
+    customerName: 'Webhook',
+    customerEmail: 'webhook@test.com',
+    customerPhone: '',
+    artwork: 'Prueba',
+    size: '30x45',
+    price: 1000,
+    paymentId: String(paymentId),
+  })
+
+  return NextResponse.json({
+    success: true,
+  })
+
+}
